d772859f373c1ca621e08fb39b4777d554aa37ce
Jamie Carter <jc2820@hotmail.com>
2020-01-15 15:42:31 +0000

    add some form elements to form.js

--- src/Form.js
+++ src/Form.js
@@ -7,7 +7,7 @@ import "./Form.css";
 const Form = (props) => {
   const [ userData, setUserData ] = React.useState(null);
   React.useEffect(() => {
-    const url = `https://api.github.com/users/${username}?access_token=${process.env.ACCESS.GITHUB.REACT_APP_GITHUB_TOKEN}`;
+    const url = `https://api.github.com/users/${username}?access_token=${process.env.REACT_APP_GITHUB_TOKEN}`;
     getUserData(url).then(data => {console.log(data); setUserData(data)});
   }, []);
     if (!userData) {
@@ -16,9 +16,24 @@ const Form = (props) => {
 
 
 return (
-  <div>
-  <h1>LIFE</h1>
-  </div>
+  <section>
+    <fieldset>
+      <form>
+        <legend>GAME OF LIFE</legend>
+        <label htmlFor="user-form">Username</label>
+        <input 
+          id="user-form"
+          type="text"
+          value={username}
+          onChange={event => setUsername(event.target.value)}
+        />
+        <button onClick={submit}>
+          Enter
+        </button>
+      </form>
+    </fieldset>
+  </section>
+
 )
 };
 
