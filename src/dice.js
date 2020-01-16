24cd09ef348df1dbe5b885320d597f0c5338882c
Jamie Carter <jc2820@hotmail.com>
2020-01-16 12:00:02 +0000

    adds dice component to game page

---
+++ src/dice.js
@@ -0,0 +1,27 @@
+import React from "react"
+import "./dice.css";
+
+const Dice = () => {
+    const [roll, setRoll] = React.useState(null);
+
+    const rollDice = (event, max, min) => {
+        event.preventDefault();
+        min = 1;
+        max = 7;
+        setRoll(Math.floor(Math.random() * (max - min)) + min)
+    }
+    
+
+    return (
+        <div className="dice-box">
+            <p className="dice-value">
+                {roll}
+            </p>
+            🎲<button onClick={rollDice}>
+                Roll Dice
+            </button>
+        </div>
+    )
+}
+
+export default Dice;
