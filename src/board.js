e20ea7dee4bd90dad5940f1ea0d50e1b0ed3c7a5
Bethany Scott <bethanyioscott@hotmail.co.uk>
2020-01-16 15:23:51 +0000

    Set rest squares on board, add initial styling

--- src/board.js
+++ src/board.js
@@ -2,14 +2,12 @@ import React from "react";
 import "./board.css";
 
 const Board = ({avatar}) => {
-    const [ position, setPosition ] = React.useState(1); 
+    const [ position, setPosition ] = React.useState(1);
     const [ key, setKey ] = React.useState(null);
-    
+
     React.useEffect(() => {
         const storeKey = event => setKey(event.key);
         window.addEventListener("keydown", storeKey);
-
-        
 
         if (key === "ArrowLeft") {
             setPosition(position - 1)
@@ -17,50 +15,150 @@ const Board = ({avatar}) => {
         } else if (key === "ArrowRight"){
             setPosition(position + 1)
             setKey(null)
-        } 
+        }
         console.log(position);
         return () => window.removeEventListener("keydown", storeKey);
-        
+
     }, [key, setKey, position, setPosition])
 
-    
+
 
      // const movePieceLeft = event  => {
     //     event.preventDefault();
-       
+
     // }
     // const movePieceRight = event => {
     //     event.preventDefault();
     // }
-    
-    
-    
+
+
+
    return (
        <section className="board">
             <div className="row">
-                <div className = "square"> 
-                    <img alt="github avatar" src={avatar} />
+                <div className = "rest-square">
+
                 </div>
-                <div className = "square"> 
-                
+                <div className = "square">
+
                 </div>
-                <div className = "square"> 
-                
+                <div className = "rest-square">
+
                 </div>
-                <div className = "square"> 
-                
+                <div className = "square">
+
                 </div>
-                <div className = "square"> 
-                
+                <div className = "rest-square">
+
                 </div>
-                <div className = "square"> 
+                <div className = "square">
+
+                </div>
+            </div>
+            <div className="row">
+                <div className = "square">
+
+                </div>
+                <div className = "rest-square">
+
+                </div>
+                <div className = "square">
+
+                </div>
+                <div className = "rest-square">
+
+                </div>
+                <div className = "square">
+
+                </div>
+                <div className = "rest-square">
+
+                </div>
+            </div>
+            <div className="row">
+                <div className = "rest-square">
+
+                </div>
+                <div className = "square">
+
+                </div>
+                <div className = "rest-square">
+
+                </div>
+                <div className = "square">
+
+                </div>
+                <div className = "rest-square">
+
+                </div>
+                <div className = "square">
+
+                </div>
+            </div>
+            <div className="row">
+                <div className = "square">
+
+                </div>
+                <div className = "rest-square">
+
+                </div>
+                <div className = "square">
+
+                </div>
+                <div className = "rest-square">
+
+                </div>
+                <div className = "square">
+
+                </div>
+                <div className = "rest-square">
+
+                </div>
+            </div>
+            <div className="row">
+                <div className = "rest-square">
+
+                </div>
+                <div className = "square">
+
+                </div>
+                <div className = "rest-square">
+
+                </div>
+                <div className = "square">
+
+                </div>
+                <div className = "rest-square">
+
+                </div>
+                <div className = "square">
+
+                </div>
+            </div>
+            <div className="row">
+                <div className = "square">
+                <img alt="github avatar" src={avatar} />
+                </div>
+                <div className = "rest-square">
+
+                </div>
+                <div className = "square">
+
+                </div>
+                <div className = "rest-square">
+
+                </div>
+                <div className = "square">
+
+                </div>
+                <div className = "rest-square">
 
                 </div>
             </div>
        </section>
    )
-   
-    
-} 
+
+
+}
 
 export default Board;
