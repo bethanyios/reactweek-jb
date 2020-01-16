1a9584e5767a0e74d4ad28e9ec839b7f5fa49913
Jamie Carter <jc2820@hotmail.com>
2020-01-16 10:04:08 +0000

    adds search for github user on click

--- src/Form.js
+++ src/Form.js
@@ -1,40 +1,53 @@
 import React from "react";
-import getUserData from "./utils/getUserData"
+import {getUserData} from "./utils/getUserData"
 import "./Form.css";
 
-// const Form = ({ username, setUsername, userData, setUserData, setCurrentPage }) => {
 
-const Form = (props) => {
-  const [ userData, setUserData ] = React.useState(null);
-  React.useEffect(() => {
-    const url = `https://api.github.com/users/${username}?access_token=${process.env.REACT_APP_GITHUB_TOKEN}`;
-    getUserData(url).then(data => {console.log(data); setUserData(data)});
-  }, []);
-    if (!userData) {
-      return <h3>...Loading</h3>;
-    }
+const Form = () => {
+  const [ userData, setUserData ] = React.useState({ name: "Jamie", avatarUrl: "https://source.unsplash.com/300x300/?pizza" });
+  const [ username, setUsername ] = React.useState("jc2820")
+  const url = `https://api.github.com/users/${username}?access_token=${process.env.REACT_APP_GITHUB_TOKEN}`;
 
-
+  const submitUser = event => {
+    event.preventDefault();
+    getUserData(url)
+    .then(response => response.json())
+    .then(data => {
+      setUserData({ name: data.name, avatarUrl: data.avatar_url })
+    })
+  }
+  
 return (
   <section>
-    <fieldset>
+    
       <form>
+      <fieldset>
         <legend>GAME OF LIFE</legend>
-        <label htmlFor="user-form">Username</label>
+        
+        <label htmlFor="user-form">Enter your github username:</label>
+        <div id="user-entry">
         <input 
           id="user-form"
           type="text"
-          value={username}
-          onChange={event => setUsername(event.target.value)}
+          placeholder="@"
+          onChange={event => setUsername(event.target.value)}          
         />
-        <button onClick={submit}>
+        <button onClick={submitUser}>
           Enter
         </button>
+        </div>
+        <img alt="github avatar" src={userData.avatarUrl} />
+            <h2>{userData.name}</h2>  
+        <button>
+          Start Game
+        </button>
+        </fieldset>
       </form>
-    </fieldset>
+    
   </section>
 
 )
 };
+
 
 export default Form;
