ac113283d231d066a6275f394b7f2182362eb468
Bethany Scott <bethanyioscott@hotmail.co.uk>
2020-01-17 11:53:06 +0000

    Refactor instructions list to <ol> tag and amend the css on fieldset for viewport height

--- src/board.js
+++ src/board.js
@@ -4,7 +4,6 @@ import "./board.css";
 
 const Board = ({avatar}) => {
     const [ position, setPosition ] = React.useState(0);
-    const [ key, setKey ] = React.useState(null);
 
     React.useEffect(() => {
        const movePosition = event => {
@@ -14,31 +13,9 @@ const Board = ({avatar}) => {
            setPosition(position + 1)
          }
        };
-
        window.addEventListener("keydown", movePosition);
-
        return () => window.removeEventListener("keydown", movePosition);
-
      }, [position, setPosition]);
-
-    //
-    // React.useEffect(() => {
-    //     const storeKey = event => setKey(event.key);
-    //     window.addEventListener("keydown", storeKey);
-    //
-    //     if (key === "ArrowLeft") {
-    //         setPosition(position - 1)
-    //         setKey(null)
-    //     } else if (key === "ArrowRight"){
-    //         setPosition(position + 1)
-    //         setKey(null)
-    //     }
-    //     console.log(position);
-    //     return () => window.removeEventListener("keydown", storeKey);
-    //
-    //
-    //
-    // }, [key, setKey, position, setPosition])
 
 return (
 Array.from ({length : 25}, (x,i) => {
@@ -47,7 +24,6 @@ Array.from ({length : 25}, (x,i) => {
     {position === i ? <Avatar avatar = {avatar} /> : null}
     </div>
   )})
-
 )
 }
 
